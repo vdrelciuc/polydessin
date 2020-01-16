8937bab6e11a52f5d20f476c306fe8136159d510
JoeA <joe.abdo@polymtl.ca>
2020-01-16 14:13:24 -0500

    Modified Skeleton

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -10,7 +10,7 @@ import { IndexService } from '../../services/index/index.service';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    readonly title: string = 'LOG2990';
+    readonly title: string = 'LOG3900';
     message = new BehaviorSubject<string>('');
 
     constructor(private basicService: IndexService) {
