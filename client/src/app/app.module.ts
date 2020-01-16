274d913a3dabc14fe7084877e125a1e56a467fad
Vlad Drelciuc <vlad.drelciuc@polymtl.ca>
2020-01-16 17:42:17 -0500

    wd - create basic components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, WorkspaceComponent, SidebarComponent, CanvasComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
