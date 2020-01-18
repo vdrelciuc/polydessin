6105a112702665b1598b8e8e1b5b4b625a9740f9
Vlad Drelciuc <vlad.drelciuc@polymtl.ca>
2020-01-18 14:44:23 -0500

    Workspace redesign and Material integration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './components/app/app.component';
-import { WorkspaceComponent } from './components/workspace/workspace.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { OptionPannelComponent } from './components/option-pannel/option-pannel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './components/app/app.component';
+import { OptionPannelComponent } from './components/option-pannel/option-pannel.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+
+import { AngularMaterialModule } from './modules/angular-material.module'
 
 @NgModule({
     declarations: [AppComponent, WorkspaceComponent, SidebarComponent, OptionPannelComponent],
-    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AngularMaterialModule],
     providers: [],
     bootstrap: [AppComponent],
 })
