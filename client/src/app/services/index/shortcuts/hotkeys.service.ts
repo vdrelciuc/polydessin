0e6d889f6baba4672a0753429a48d397f81db504
JoeA <joe.abdo@polymtl.ca>
2020-01-19 10:01:07 -0500

    Shortcut service (hotkeys) created

---
+++ client/src/app/services/index/shortcuts/hotkeys.service.ts
@@ -0,0 +1,52 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+import { EventManager } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+
+// How to user hotkey service
+// 1- Create in the drawable tool component's constructor (public hotkeyService: HotkeysService)
+// 2- Add the key listeners
+// shift.j = shift + j , control can be used too
+      // hotkeys.addShortcut({ keys: 'shift.j', description: 'Function description' }).subscribe(   (event)=>{
+      //    what to do when the event happens
+      //   });)
+
+interface Options {
+  element: any; // Creates an error with HTMLElement
+  keys: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HotkeysService {
+
+    defaults: Partial<Options> = {
+      element: this.document
+    }
+
+    constructor(
+      private eventManager: EventManager,
+      @Inject(DOCUMENT) private document: Document) {
+    }
+
+    addShortcut(options: Partial<Options>): Observable<Event> {
+      const merged = { ...this.defaults, ...options };
+      const event = `keydown.${merged.keys}`;
+
+      return new Observable((observer) => {
+          const handler = (e: Event) => {
+            e.preventDefault()
+            observer.next(e);
+          };
+
+          const dispose = this.eventManager.addEventListener(
+            merged.element, event, handler
+          );
+          return () => {
+            dispose();
+          };
+      })
+    }
+}
