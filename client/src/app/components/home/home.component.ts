654b2bf8fa6ca8bd4d5daa8e69c0ded0d9ea9008
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-19 15:56:49 -0500

    Added Bootstrap and added the home page option to the home component

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  options: string[] =
+    ['Creer un nouveau dessin', 'Ouvrir la Galerie de dessins', "Afficher le guide d'Utilisation ", 'Continuer un dessin'];
   constructor() { }
 
   ngOnInit() {
