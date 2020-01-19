0183a4853d17d942aa5208b0235f8dc125d7bd09
JoeA <joe.abdo@polymtl.ca>
2020-01-19 15:08:07 -0500

    Creating a tool handler

--- client/src/app/services/index/tool/tool.service.ts
+++ client/src/app/services/index/tool/tool.service.ts
@@ -1,22 +1,43 @@
 import { Injectable } from '@angular/core';
-import { LineComponent } from 'src/app/components/app/line/line.component';
 import { DrawableService } from '../drawable/drawable.service';
 import { LineService } from './line/line.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ToolService {
 
-  tools = new Map<string, DrawableService>();
+  private tools: Map<string, DrawableService>;
+  private currentTool: DrawableService;
 
   constructor() { // Add every tool that is going to be used with it's name format (name, toolService)
-      this.tools.set(LineComponent.name, LineService);
+    this.tools = new Map<string, DrawableService>();
+    this.tools.set(LineService.serviceName, new LineService());
+
+      // Initialize currentTool as the selector(mouse)
+    this.initialize();
   }
 
   initialize(): void {
-    for(const element of this.tools) {
-      element.initialize();
+    for (const element of this.tools) {
+      element[1].initialize();
+    }
+  }
+
+  getTool(toFind: string): DrawableService | undefined {
+    if (this.tools.has(toFind)) {
+      return this.tools.get(toFind);
+    }
+    return undefined;
+  }
+
+  getCurrentTool(): DrawableService { return this.currentTool; }
+
+  setCurrentTool(toSet: string): void  {
+    const foundTool = this.getTool(toSet);
+    if (foundTool !== undefined) {
+      this.currentTool = foundTool;
     }
   }
 }
