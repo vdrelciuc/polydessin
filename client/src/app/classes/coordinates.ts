1cd128093764cd9ef6159876562bd517f4fa9b59
JoeA <joe.abdo@polymtl.ca>
2020-01-19 15:52:28 -0500

    Creating a drawable abstract class

---
+++ client/src/app/classes/coordinates.ts
@@ -0,0 +1,11 @@
+import { ThrowStmt } from '@angular/compiler';
+
+export class Coords {
+  x: number;
+  y: number;
+
+  constructor(x: number, y:number) {
+    this.x = x;
+    this.y = y;
+  }
+}
--- client/src/app/services/index/drawable/drawable.service.ts
+++ client/src/app/services/index/drawable/drawable.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Coords } from 'src/app/classes/coordinates';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,18 @@ import { Injectable } from '@angular/core';
 export abstract class DrawableService {
 
   abstract initialize(): void;
+
+  getCoords(pointer: MouseEvent): Coords {
+    return new Coords(pointer.clientX, pointer.clientY);
+  }
+
+  canDraw(canvas: HTMLElement, pointer: MouseEvent): boolean {
+    return (
+        // To change depending on what is the canvas' type and how to get it's dimensions
+      true
+    );
+  }
+
+  abstract onMouseInCanvas(event: MouseEvent): void;
+  abstract onMouseOutCanvas(event: MouseEvent): void;
 }
