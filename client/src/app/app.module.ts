f4cac10ec5f7a486022a148d99551a6395127043
William Decoste <william.decoste@polymtl.ca>
2020-01-18 20:37:35 -0500

    Added WorkspaceService with distinguishable canvas on same color

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './components/app/app.component';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CanvasComponent } from './components/canvas/canvas.component';
+import { WorkspaceService } from './services/workspace.service';
 
 @NgModule({
     declarations: [AppComponent, WorkspaceComponent, SidebarComponent, CanvasComponent],
     imports: [BrowserModule, HttpClientModule],
-    providers: [],
+    providers: [WorkspaceService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/components/canvas/canvas.component.ts
+++ client/src/app/components/canvas/canvas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkspaceService } from 'src/app/services/workspace.service';
 
 @Component({
   selector: 'app-canvas',
@@ -9,9 +10,9 @@ export class CanvasComponent implements OnInit {
   width: number;
   height: number;
 
-  backgroundColorHex = 'FFF2FF';
+  backgroundColorHex = 'FFFFFF';
 
-  constructor() { }
+  constructor(private workspaceService: WorkspaceService) { }
 
   ngOnInit(width: number = 800, height: number = 400) {
     this.width = width;
