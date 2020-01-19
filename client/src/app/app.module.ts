c50b67c7ffa150d09b5105cb1e27630c70bbe4b7
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-19 15:25:18 -0500

    AB : creating the home folder for the component and adding the component to the app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HomeComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
