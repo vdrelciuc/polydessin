62924c5b17f78f91b1f7d3a622d207745e1081bd
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-20 01:27:00 -0500

    added properties to the component finished the basic skeleton and styling of the entry added a copyright free Logo to the home page

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   options: string[] =
     ['Creer un nouveau dessin', 'Ouvrir la Galerie de dessins', "Afficher le guide d'Utilisation ", 'Continuer un dessin'];
+  messageAccueil : string = "Bienvenue a SketchPad Clone";
+  messageDescriptif : string = "A tout dessin un artiste, et cet artiste, c'est vous!";
   constructor() { }
 
   ngOnInit() {
