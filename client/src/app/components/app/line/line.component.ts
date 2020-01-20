74bfb437ed4506a1ae91b6437a6bb2dae7c4a265
JoeA <joe.abdo@polymtl.ca>
2020-01-20 10:23:16 -0500

    Integrating shortcuts to Line

--- client/src/app/components/app/line/line.component.ts
+++ client/src/app/components/app/line/line.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HotkeysService } from 'src/app/services/index/shortcuts/hotkeys.service';
 
 @Component({
   selector: 'app-line',
@@ -11,10 +12,28 @@ export class LineComponent implements OnInit {
   readonly name: string = 'Line';
   specificationForm: FormGroup;
 
-  constructor() {
+  constructor(
+    private shortcuts: HotkeysService
+  ) {
+    this.setupShortcuts();
   }
 
   ngOnInit(): void {
+
+  }
+
+  setupShortcuts(): void {
+    this.shortcuts.addShortcut({ keys: 'backspace', description: 'Remove last point' }).subscribe(
+      (event)=>{
+        console.log('Backspace pressed - Remove last point');
+      }
+    );
+
+    this.shortcuts.addShortcut({ keys: 'escape', description: 'Cancel current line' }).subscribe(
+      (event)=>{
+        console.log('Escape pressed - Cancel current line');
+      }
+    );
   }
 
 
