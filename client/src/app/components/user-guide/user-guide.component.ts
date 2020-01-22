3ec83feb4b9252b0bb29e05a723a21de9bca4a97
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-21 20:33:19 -0500

    created JSON Template for the guide to set a binding rule from the .ts file to the html file

--- client/src/app/components/user-guide/user-guide.component.ts
+++ client/src/app/components/user-guide/user-guide.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-guide.component.scss']
 })
 export class UserGuideComponent implements OnInit {
+  // catégorie[position de la catégorie][positionDeL'outil].champs de l'outil
+  categories : any[] = [
+    {
+      type: {
+        nom: 'Outils',
+        elements: [
+          { nom: 'Pinceau', description: 'nonDisponible'},
+          { nom: 'Crayon', description: 'nonDisponible'},
+          { nom: 'Ligne', description: 'nonDisponible'},
+          { nom: 'Rectangle', description: 'nonDisponible'},
+          { nom: 'Couleur', description: 'nonDisponible'}
+        ]
+      }
+    },
+
+    {
+      type: {
+        nom: 'Fichier',
+        elements: [
+          { nom: 'sauvegarde', description: 'nonDisponible' },
+          { nom: 'une autre type de sauvegarde', description: 'nonDisponible' }
+        ]
+      }
+    }
+  ];
 
   constructor() { }
 
