f1f6c4ad213124759a39cbc8a7ea0b00125fd336
JoeA <joe.abdo@polymtl.ca>
2020-01-22 16:07:23 -0500

    Adding SVG properties to the line service

---
+++ client/src/app/classes/svg-html-properties.ts
@@ -0,0 +1,25 @@
+export enum SVGProperties {
+
+  fill = 'fill',
+  container = 'g',
+  height = 'height',
+  hyperlink = 'a',
+  opacity = 'opacity',
+  width = 'width',
+
+  // Circle
+  centerX = 'cx',
+  centerY = 'cy',
+  radius = 'r',
+
+  // Ellipse
+  radiusX = 'rx',
+  radiusY = 'ry',
+
+  // Line
+  pointsList = 'points',
+
+  // Point
+  x = 'x',
+  y = 'y'
+}
--- client/src/app/services/index/drawable/line/line.service.ts
+++ client/src/app/services/index/drawable/line/line.service.ts
@@ -4,6 +4,7 @@ import { Stack } from 'src/app/classes/stack';
 import { Shape } from 'src/app/classes/shape';
 import { CoordinatesXY } from 'src/app/classes/coordinates-x-y';
 import { SVGService } from '../../svg/svg.service';
+import { DrawablePropertiesService } from '../properties/drawable-properties.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +13,33 @@ export class LineService extends DrawableService {
 
   private isDrawing: boolean;
   private lineIsDone: boolean;
+  private lineStarted: boolean;
   private points: Stack<CoordinatesXY>;
-  private currentElement: SVGGElement;
+  private currentElement: SVGPolylineElement;
+  private manipulator: Renderer2
 
-  constructor(private manipilator: Renderer2) {
+  constructor(private properties: DrawablePropertiesService) {
     super();
   }
 
   initialize(): void {
     throw new Error("Method not implemented.");
   }
+
+
+  onMouseMove(event: MouseEvent): void {
+    if (!this.lineIsDone) {
+      this.preview(event.clientX, event.clientY);
+    }
+  }
+
   onMouseInCanvas(event: MouseEvent): void {
+    // Change the pointer's appearance
     throw new Error("Method not implemented.");
   }
+
   onMouseOutCanvas(event: MouseEvent): void {
+    // Change the pointer's appearance
     throw new Error("Method not implemented.");
   }
 
@@ -34,6 +48,12 @@ export class LineService extends DrawableService {
     // Should make modifications
     // Screen's X and Y are not the same as the canvas'
 
+    if (this.properties.isJunctionADot()) {
+      if (!this.lineStarted) {
+        this.currentElement = this.manipulator.createElement('g');
+      }
+      this.manipulator.appendChild(this.currentElement, '')
+    }
     this.points.push_back(new CoordinatesXY(onScreenX, onScreenY));
   }
 
@@ -44,7 +64,7 @@ export class LineService extends DrawableService {
     throw new Error("Method not implemented.");
   }
 
-  onDblClick(event: MouseEvent): void { // Should end line
+  onDoubleClick(event: MouseEvent): void { // Should end line
     if (this.isDrawing) {
         this.isDrawing = false;
         if (!this.lineIsDone) {
@@ -54,6 +74,11 @@ export class LineService extends DrawableService {
         this.currentStack.addElement()
         this.points.clear();
     }
-}
+  }
+
+  preview(x: number, y: number): void {
+      this.manipulator.setAttribute(this.currentElement, )
+  }
+
 
 }
