dd98d32d38217ef346787c46a7940ebb94be654c
JoeA <joe.abdo@polymtl.ca>
2020-01-24 16:26:30 -0500

    Testing hotkeys

--- client/src/app/services/index/shortcuts/hotkeys.service.spec.ts
+++ client/src/app/services/index/shortcuts/hotkeys.service.spec.ts
@@ -9,4 +9,22 @@ describe('HotkeysService', () => {
     const service: HotkeysService = TestBed.get(HotkeysService);
     expect(service).toBeTruthy();
   });
+
+  it('should be created', () => {
+    const service: HotkeysService = TestBed.get(HotkeysService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should create shortcut', () => {
+    const service: HotkeysService = TestBed.get(HotkeysService);
+    service.addShortcut({ keys: 'shift.j', description: 'Function description' });
+    expect(service.defaults.description).toBe('Function description');
+  });
+
+  it('should change shortcut', () => {
+    const service: HotkeysService = TestBed.get(HotkeysService);
+    service.addShortcut({ keys: 'shift.j', description: 'Function description' });
+    service.addShortcut({ keys: 'shift.j', description: 'Function Changed' });
+    expect(service.defaults.description).toBe('Function Changed');
+  });
 });
