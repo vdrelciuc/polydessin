d6150042e10d3a6bed47d40337ec0536bd3327ee
JoeA <joe.abdo@polymtl.ca>
2020-01-24 16:37:44 -0500

    Testing workspace service

--- client/src/app/classes/thickness-validator.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ThicknessValidator } from './thickness-validator';
-
-describe('ThicknessValidator', () => {
-  it('should create an instance', () => {
-    expect(new ThicknessValidator()).toBeTruthy();
-  });
-});
--- client/src/app/services/workspace.service.spec.ts
+++ client/src/app/services/workspace.service.spec.ts
@@ -9,4 +9,38 @@ describe('WorkspaceService', () => {
     const service: WorkspaceService = TestBed.get(WorkspaceService);
     expect(service).toBeTruthy();
   });
+
+  it('should set new color', () => {
+    const service: WorkspaceService = TestBed.get(WorkspaceService);
+    service.setBackgroundColorHex('898989');
+    expect(service.checkIfSameBackgroundColor('898989')).toBe(true);
+  });
+
+  it('should be same color', () => {
+    const service: WorkspaceService = TestBed.get(WorkspaceService);
+    expect(service.checkIfSameBackgroundColor('808080')).toBe(true);
+  });
+
+
+  it('should not be same color/ valid color', () => {
+    const service: WorkspaceService = TestBed.get(WorkspaceService);
+    expect(service.checkIfSameBackgroundColor('809980')).toBe(false);
+  });
+
+  it('should not be same color/ not valid color', () => {
+    const service: WorkspaceService = TestBed.get(WorkspaceService);
+    expect(service.checkIfSameBackgroundColor('i am here')).toBe(false);
+  });
+
+  it('should assign color', () => {
+    const service: WorkspaceService = TestBed.get(WorkspaceService);
+    service.setBackgroundColorHex('898989');
+    expect(service.checkIfSameBackgroundColor('898989')).toBe(true);
+  });
+
+  it('should not assign color (not valid)', () => {
+    const service: WorkspaceService = TestBed.get(WorkspaceService);
+    service.setBackgroundColorHex('i am here');
+    expect(service.checkIfSameBackgroundColor('808080')).toBe(true);
+  });
 });
