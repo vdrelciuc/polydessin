3de66e296bc550637f1ebb6e031fb15e71bdd3b4
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-26 15:07:07 -0500

    added function to the guide component to find indexes and to navigate from a guide element to another

--- client/src/app/components/user-guide/user-guide.component.ts
+++ client/src/app/components/user-guide/user-guide.component.ts
@@ -39,11 +39,51 @@ export class UserGuideComponent implements OnInit {
    **/
 
 
+  findIndex(nom :string) : any[] {
+    if(nom === 'Bienvenue' )
+      return [0,0,true];
+    for (let i : number = 0 ; i< this.categories.length ; ++i){
+      for (let j : number = 0 ; j < this.categories[j].type.elements.length ; ++j){
+        if (nom === this.categories[i].type.elements[j].nom){
+          if (i=== (this.categories[i].type.elements.length -1)){
+            return [j,i,true];
+          }
+          else{
+            return [j,i,false];
+          }
+        }
+      }
+    }
+    return [0,0,true];
+  }
+
+  getNextElement(indexes : any[]){
+    if (indexes[2]==true){
+      indexes[0]++;
+      indexes[1]=0;
+    }
+    else
+      indexes[1]++;
+    let newElement : string = this.categories[indexes[0]].type.elements[indexes[1]].nom;
+    this.setCurrentSubCategorie(newElement);
+  }
+
+  getPreviousElement(indexes : any[]){
+    if (indexes[1]==0){
+      indexes[0]--;
+      indexes[1]= (this.categories[indexes[0]].type.elements.length -1);
+    }
+    else
+      indexes[1]--;
+    let newElement : string = this.categories[indexes[0]].type.elements[indexes[1]].nom;
+    this.setCurrentSubCategorie(newElement);
+  }
+
   /**
    * Attributes
    **/
   private _currentCategorie : string ;
-  private _currentSubCategorie : string ;
+  private _currentSubCategorie : string = "Bienvenue";
   private _currentDescription : string ;
 
   welcomeObject : object={ nom: 'Bienvenue', description: 'nonDisponible'};
