6ffaa51c0b890e2ffea11dfb771c90259de4e5aa
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-27 17:16:10 -0500

    Added code for crayon vue

--- client/src/app/components/app/app-routing.module.ts
+++ client/src/app/components/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {UserGuideComponent} from '../user-guide/user-guide.component';
 import { BienvenueGuideComponent } from '../guideTemplaates/bienvenue-guide/bienvenue-guide.component';
 import { LigneGuideComponent } from '../guideTemplaates/ligne-guide/ligne-guide.component';
 import { PinceauGuideComponent } from '../guideTemplaates/pinceau-guide/pinceau-guide.component';
+import { CrayonGuideComponent } from '../guideTemplaates/crayon-guide/crayon-guide.component';
 
 const routes: Routes = [
   {path : 'userGuide', component : UserGuideComponent},
   {path : 'userGuide/Bienvenue', component : BienvenueGuideComponent},
   {path : 'userGuide/Ligne' , component : LigneGuideComponent},
-  {path : 'userGuide/Pinceau' , component : PinceauGuideComponent }
+  {path : 'userGuide/Pinceau' , component : PinceauGuideComponent },
+  {path : 'userGuide/Crayon', component : CrayonGuideComponent}
 ]
 @NgModule({
   imports : [RouterModule.forRoot(routes)],
@@ -17,4 +19,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule{}
-export const routingComponents = [UserGuideComponent,BienvenueGuideComponent,LigneGuideComponent, PinceauGuideComponent]
+export const routingComponents = [UserGuideComponent,BienvenueGuideComponent,LigneGuideComponent, PinceauGuideComponent, CrayonGuideComponent]
