9181e26a531930b9689119e14299f947e9b3f1f1
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-27 17:05:44 -0500

    Vue Bienvenue 'finished'

--- client/src/app/components/app/app-routing.module.ts
+++ client/src/app/components/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import {UserGuideComponent} from '../user-guide/user-guide.component';
 import { BienvenueGuideComponent } from '../guideTemplaates/bienvenue-guide/bienvenue-guide.component';
+import { LigneGuideComponent } from '../guideTemplaates/ligne-guide/ligne-guide.component';
 
 const routes: Routes = [
   {path : 'userGuide', component : UserGuideComponent},
-  {path : 'userGuide/Bienvenue', component : BienvenueGuideComponent}
+  {path : 'userGuide/Bienvenue', component : BienvenueGuideComponent},
+  {path : 'userGuide/Ligne' , component : LigneGuideComponent}
 ]
 @NgModule({
   imports : [RouterModule.forRoot(routes)],
@@ -13,4 +15,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule{}
-export const routingComponents = [UserGuideComponent,BienvenueGuideComponent]
+export const routingComponents = [UserGuideComponent,BienvenueGuideComponent,LigneGuideComponent]
