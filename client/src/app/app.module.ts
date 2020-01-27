1f85855a76ffca3a66a65d84db3263097817a37f
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-01-27 14:33:52 -0500

    added components for the first category of the user guide

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,14 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatListModule} from '@angular/material/list';
 import {AppRoutingModule, routingComponents} from './components/app/app-routing.module';
+import { PinceauGuideComponent } from './components/guideTemplaates/pinceau-guide/pinceau-guide.component';
+import { CrayonGuideComponent } from './components/guideTemplaates/crayon-guide/crayon-guide.component';
+import { LigneGuideComponent } from './components/guideTemplaates/ligne-guide/ligne-guide.component';
+import { RectangleGuideComponent } from './components/guideTemplaates/rectangle-guide/rectangle-guide.component';
+import { CouleurGuideComponent } from './components/guideTemplaates/couleur-guide/couleur-guide.component';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, routingComponents],
+    declarations: [AppComponent, HomeComponent, routingComponents, PinceauGuideComponent, CrayonGuideComponent, LigneGuideComponent, RectangleGuideComponent, CouleurGuideComponent],
     imports: [BrowserModule, HttpClientModule, MatExpansionModule, BrowserAnimationsModule, MatListModule
     , AppRoutingModule],
     providers: [],
---
+++ client/src/app/components/guideTemplaates/pinceau-guide/pinceau-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pinceau-guide',
+  templateUrl: './pinceau-guide.component.html',
+  styleUrls: ['./pinceau-guide.component.scss']
+})
+export class PinceauGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
