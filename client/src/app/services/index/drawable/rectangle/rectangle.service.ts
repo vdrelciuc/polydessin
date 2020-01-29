a16353fc929b64d28b689359deae9948c77930f7
William Decoste <william.decoste@polymtl.ca>
2020-01-29 12:55:27 -0500

    Shape name appears in parameter

--- client/src/app/services/index/drawable/rectangle/rectangle.service.ts
+++ client/src/app/services/index/drawable/rectangle/rectangle.service.ts
@@ -15,14 +15,16 @@ export class RectangleService extends DrawableService{
   borderColor: string;
   fillColor: string;
   opacity: string;
-  private isChanging: boolean;
-  // private points: Stack<CoordinatesXY>;
-  // private circles: Stack<SVGCircleElement>;
+  
   private mousePosition: Coords;
   private shapeOrigin: Coords;
-  private rectangle: SVGRectElement;
-  private subElement: SVGGElement;
+
+  private isChanging: boolean;
   private shiftPressed: boolean;
+
+  private rectangle: SVGRectElement;
+  private text: SVGTextElement;
+  private subElement: SVGGElement;
 
   constructor() { 
     super();
@@ -62,6 +64,7 @@ export class RectangleService extends DrawableService{
   }
   onMouseRelease(event: MouseEvent): void {
     this.isChanging = false;
+    this.manipulator.removeChild(this.subElement, this.text); // Will be destroyed automatically
   }
   onMouseMove(event: MouseEvent): void {
     if(this.isChanging) {
@@ -76,14 +79,17 @@ export class RectangleService extends DrawableService{
     // throw new Error("Method not implemented.");
   }
   onKeyPressed(event: KeyboardEvent): void {
-    if(event.shiftKey && !this.shiftPressed) {
+    if(event.shiftKey && !this.shiftPressed && this.isChanging) {
+      console.log("Shift pressed");
       this.shiftPressed = true;
       this.updateSize(this.mousePosition.x - this.shapeOrigin.x, this.mousePosition.y - this.shapeOrigin.y);
     }
   }
   onKeyReleased(event: KeyboardEvent): void {
-    this.shiftPressed = false;
-    if (this.isChanging) {
+    if(!event.shiftKey) {
+      console.log("Shift released");
+      this.shiftPressed = false;
+      if (this.isChanging)
       this.updateSize(this.mousePosition.x - this.shapeOrigin.x, this.mousePosition.y - this.shapeOrigin.y);
     }
   }
@@ -99,15 +105,31 @@ export class RectangleService extends DrawableService{
 
     this.manipulator.setAttribute(this.rectangle, SVGProperties.width, width.toString());
     this.manipulator.setAttribute(this.rectangle, SVGProperties.height, height.toString());
+
+    this.updateTextSize(width, height);
+  }
+
+  private updateTextSize(width: number, height: number) {
+    if(width > 40 && height > 15) {
+      this.text.innerHTML = '[Rectangle]';
+    } else {
+      this.text.innerHTML = '';
+    }
+    
+    this.manipulator.setAttribute(this.text, 'font-size', (Math.min(height, width) / 6).toString());
+    this.manipulator.setAttribute(this.text, SVGProperties.x, (this.shapeOrigin.x + width / 2).toString());
+    this.manipulator.setAttribute(this.text, SVGProperties.y, (this.shapeOrigin.y + height / 2).toString());
   }
 
   private updateProperties(): void {
+    // Creating elements
     this.subElement = this.manipulator.createElement('g', 'http://www.w3.org/2000/svg');
     this.manipulator.setAttribute(this.subElement, SVGProperties.title, Tools.Rectangle);
     this.rectangle = this.manipulator.createElement(SVGProperties.rectangle,'http://www.w3.org/2000/svg');
+    this.text = this.manipulator.createElement('text','http://www.w3.org/2000/svg');
 
+    // Adding rectangle and text properties
     this.updateSize(); // Width and height can't be tempered with in attribute
-
     this.manipulator.setAttribute(this.rectangle, SVGProperties.x, this.shapeOrigin.x.toString());
     this.manipulator.setAttribute(this.rectangle, SVGProperties.y, this.shapeOrigin.y.toString());
     this.manipulator.setAttribute(this.rectangle, SVGProperties.fill, this.fillColor);
@@ -115,7 +137,39 @@ export class RectangleService extends DrawableService{
     this.manipulator.setAttribute(this.rectangle, SVGProperties.color, this.borderColor);
     this.manipulator.setAttribute(this.rectangle, SVGProperties.opacity, this.opacity);
 
+    this.manipulator.setAttribute(this.text, SVGProperties.fill, 'white');
+    this.manipulator.setAttribute(this.text, 'text-anchor', 'middle');
+
+    // Adding elements to DOM
     this.manipulator.appendChild(this.subElement, this.rectangle);
+    this.manipulator.appendChild(this.subElement, this.text);
     this.manipulator.appendChild(this.image.nativeElement, this.subElement);
+  }
+
+  private getQuadrant(): 1 | 2 | 3 | 4 {
+    //    2 | 1
+    //   ---|---
+    //    3 | 4
+
+    let isTop = this.mousePosition.y < this.shapeOrigin.y;
+    let isLeft = this.mousePosition.x < this.shapeOrigin.x;
+
+    return isTop? (isLeft ? 2 : 1) : (isLeft ? 3 : 4);
+  }
+
+  private updateOrigin() {
+    let quadrant = this.getQuadrant();
+
+    if (quadrant === 1 || quadrant === 4) {
+      this.manipulator.setAttribute(this.rectangle, SVGProperties.x, this.shapeOrigin.x.toString());
+    } else {
+      this.manipulator.setAttribute(this.rectangle, SVGProperties.x, this.mousePosition.x.toString());
+    }
+    
+    if(quadrant === 3 || quadrant === 4) {
+      this.manipulator.setAttribute(this.rectangle, SVGProperties.y, this.shapeOrigin.x.toString());
+    } else {
+      this.manipulator.setAttribute(this.rectangle, SVGProperties.y, this.mousePosition.x.toString());
+    }
   }
 }
