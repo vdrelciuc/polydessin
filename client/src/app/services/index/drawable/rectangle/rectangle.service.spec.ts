9d8466a291ce835651042aac92e9128a7afd3876
William Decoste <william.decoste@polymtl.ca>
2020-02-02 17:50:49 -0500

    Testing Coords class

--- client/src/app/services/index/drawable/rectangle/rectangle.service.spec.ts
+++ client/src/app/services/index/drawable/rectangle/rectangle.service.spec.ts
@@ -3,10 +3,20 @@ import { TestBed } from '@angular/core/testing';
 import { RectangleService } from './rectangle.service';
 
 describe('RectangleService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let rectangleService: RectangleService;
+
+  //beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => { rectangleService = new RectangleService() });
 
   it('should be created', () => {
     const service: RectangleService = TestBed.get(RectangleService);
     expect(service).toBeTruthy();
   });
+
+  it('should set the RectangleService with its correct default attributes', () => {
+    expect(rectangleService.frenchName).toBe("Rectangle");
+    expect(rectangleService.shapeStyle).toBeTruthy();
+  })
+
 });
