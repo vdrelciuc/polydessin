c8b394ae22a57d3b0fcb1d3bbac33a762b6e32bf
JoeA <joe.abdo@polymtl.ca>
2020-02-02 20:55:06 -0500

    Testing AppComponent

--- client/src/app/components/app/app.component.spec.ts
+++ client/src/app/components/app/app.component.spec.ts
@@ -1,10 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 import { async, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 import { IndexService } from '../../services/index/index.service';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MatSidenavModule, MatSliderModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatDialogModule } from '@angular/material';
+import { OptionPannelComponent } from '../option-pannel/option-pannel.component';
+import { LineComponent } from '../line/line.component';
+import { PencilComponent } from '../pencil/pencil.component';
+import { WorkspaceComponent } from '../workspace/workspace.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CanvasComponent } from '../canvas/canvas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import SpyObj = jasmine.SpyObj;
+import { WorkingAreaComponent } from '../working-area/working-area.component';
 
 describe('AppComponent', () => {
     let indexServiceSpy: SpyObj<IndexService>;
@@ -16,9 +28,38 @@ describe('AppComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [RouterTestingModule, HttpClientModule],
-            declarations: [AppComponent],
-            providers: [{ provide: IndexService, useValue: indexServiceSpy }],
+            providers: [
+                { 
+                    provide: IndexService, 
+                    useValue: indexServiceSpy 
+                }
+            ],
+            declarations: [ 
+                AppComponent,
+                // RouterTestingModule, 
+                // HttpClientModule,
+                CanvasComponent,
+                OptionPannelComponent,
+                WorkingAreaComponent, 
+                SidebarComponent,
+                LineComponent,
+                PencilComponent,
+                WorkspaceComponent ],
+              imports: [
+                BrowserAnimationsModule,
+                MatDialogModule,
+                MatSidenavModule,
+                RouterModule.forRoot(
+                  [
+                    { path: "", component: SidebarComponent}
+                  ]
+                ),
+                FormsModule,
+                MatSliderModule,
+                MatFormFieldModule,
+                MatOptionModule,
+                MatSelectModule
+              ]
         });
     }));
 
