016df579931c415dbd37366e43c8931893cba930
JoeA <joe.abdo@polymtl.ca>
2020-02-06 17:30:39 -0500

    Testing Color.ts

--- client/src/app/classes/color.spec.ts
+++ client/src/app/classes/color.spec.ts
@@ -25,39 +25,39 @@ describe('Color', () => {
         expect(color.getRGB()).toEqual(customRgb);
     });
 
-    it('should\'t set invalid hex of 9 chars without hash', () => {
+    it('#setHex should\'t set invalid hex of 9 chars without hash', () => {
         color.setHex('111111111');
         expect(color.getHex()).toEqual(defaultColor);
     });
 
-    it('should\'t set invalid hex of 5 chars without hash', () => {
+    it('#setHex should\'t set invalid hex of 5 chars without hash', () => {
         color.setHex('11111');
         expect(color.getHex()).toEqual(defaultColor);
     });
 
-    it('should set valid hex without hash', () => {
+    it('#setHex should set valid hex without hash', () => {
         const testHex = '111111';
         color.setHex(testHex);
         expect(color.getHex()).toEqual('#' + testHex);
     });
 
-    it('should\'t set invalid hex of 9 chars with hash', () => {
+    it('#setHex should\'t set invalid hex of 9 chars with hash', () => {
         color.setHex('#11111111');
         expect(color.getHex()).toEqual(defaultColor);
     });
 
-    it('should\'t set invalid hex of 5 chars with hash', () => {
+    it('#setHex should\'t set invalid hex of 5 chars with hash', () => {
         color.setHex('#1111');
         expect(color.getHex()).toEqual(defaultColor);
     });
 
-    it('should set valid hex with hash', () => {
+    it('#setHex should set valid hex with hash', () => {
         const testHex = '#111111';
         color.setHex(testHex);
         expect(color.getHex()).toEqual(testHex);
     });
 
-    it('shouldn\'t set hex with invalid letters', () => {
+    it('#setHex shouldn\'t set hex with invalid letters', () => {
         const testHex = '#1ZZ111';
         color.setHex(testHex);
         expect(color.getHex()).toEqual(defaultColor);
@@ -67,13 +67,73 @@ describe('Color', () => {
         expect(color.getRGB()).toEqual(defaultRGB);
     });
 
-    it('shouldn\'t set rbg with invalid numbers', () => {
+    it('#setRGB shouldn\'t set rbg with invalid numbers', () => {
         color.setRGB([-10, 300, 10]);
         expect(color.getRGB()).toEqual([0, 255, 10]);
     });
 
-    it('shouldn\'t set rbg with more numbers than required', () => {
+    it('#setRGB shouldn\'t set rbg with more numbers than required', () => {
         color.setRGB([100, 100, 10, 100]);
         expect(color.getRGB()).toEqual(defaultRGB);
+    });
+
+    it('#getRedHex should get red part', () => {
+        color.setRGB([15,16,17]); // #0f1011
+        expect(color.getRedHex()).toEqual('0F');
+    });
+
+    it('#getGreenHex should get green part', () => {
+        color.setRGB([15,16,17]); // #0f1011
+        expect(color.getGreenHex()).toEqual('10');
+    });
+
+    it('#getBlueHex should get blue part', () => {
+        color.setRGB([15,16,17]); // #0f1011
+        expect(color.getBlueHex()).toEqual('11');
+    });
+
+    it('#setRedHex should set red part', () => {
+        color.setRedHex('0f'); 
+        expect(color.getRedHex()).toEqual('0F');
+    });
+
+    it('#setRedHex should set red part with empty parameter', () => {
+        color.setRedHex(''); 
+        expect(color.getRedHex()).toEqual('00');
+    });
+
+    it('#setRedHex should set red part with missing parameter', () => {
+        color.setRedHex('F'); 
+        expect(color.getRedHex()).toEqual('0F');
+    });
+
+    it('#setGreenHex should set green part', () => {
+        color.setGreenHex('10');
+        expect(color.getGreenHex()).toEqual('10');
+    });
+
+    it('#setGreenHex should set green part with empty parameter', () => {
+        color.setGreenHex(''); 
+        expect(color.getGreenHex()).toEqual('00');
+    });
+
+    it('#setGreenHex should set green part with missing parameter', () => {
+        color.setGreenHex('F'); 
+        expect(color.getGreenHex()).toEqual('0F');
+    });
+
+    it('#setBlueHex should set blue part', () => {
+        color.setBlueHex('11');
+        expect(color.getBlueHex()).toEqual('11');
+    });
+
+    it('#setBlueHex should set blue part with empty parameter', () => {
+        color.setBlueHex(''); 
+        expect(color.getBlueHex()).toEqual('00');
+    });
+
+    it('#setBlueHex should set blue part with missing parameter', () => {
+        color.setBlueHex('F'); 
+        expect(color.getBlueHex()).toEqual('0F');
     });
 });
