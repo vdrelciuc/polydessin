451c2b23a16fa4d7c8f489251cfe141e2c306c79
JoeA <joe.abdo@polymtl.ca>
2020-02-06 18:06:40 -0500

    Testing CoordinatedXY.ts

--- client/src/app/classes/coordinates.spec.ts
+++ client/src/app/classes/coordinates.spec.ts
@@ -1,13 +1,57 @@
 import { Coords } from './coordinates';
+import { Renderer2 } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 describe('Coords', () => {
 
   let point: Coords;
+  const testValue = 100;
 
-  beforeEach( () => { point = new Coords(10, 10); } );
+  beforeEach( () => { 
+    TestBed.configureTestingModule({
+      declarations: [  ],
+      providers: [
+        {
+          provide: Renderer2,
+          useValue: {
+            createElement: () => null,
+            setAttribute: () => null,
+            appendChild: () => null,
+            listen: () => null,
+            nativeElement: {
+              useValue: {
+                getBoundingClientRect: () => 100,
+              }
+            }
+          },
+        },
+      ],
+    }).compileComponents();
+    point = new Coords(10, 10); 
+  });
 
   it('should create a Coords', () => {
     expect(new Coords(10, 10)).toBeTruthy();
+  });
+
+  it('#getCoords should get mouse coords', () => {
+    const pointer = Coords.getCoords(new MouseEvent('mouseup', {
+      clientX: testValue,
+      clientY: testValue
+    }));
+    expect(pointer.x).toEqual(testValue);
+    expect(pointer.y).toEqual(testValue);
+  });
+
+  it('#getEffectiveCoords should return effective coordinates', () => {
+    const pointer = Coords.getEffectiveCoords(
+      TestBed.get(Renderer2), new MouseEvent('mouseup', {
+        clientX: testValue * 2,
+        clientY: testValue * 2
+      })
+    );
+    expect(pointer.x).toEqual(testValue);
+    expect(pointer.y).toEqual(testValue);
   });
 
   it('should be able to access coordinates', () => {
@@ -27,6 +71,5 @@ describe('Coords', () => {
     expect(point.getQuadrant(new Coords(15, 15))).toBe(2);
     expect(point.getQuadrant(new Coords(15, 5))).toBe(3);
     expect(point.getQuadrant(new Coords(5, 5))).toBe(4);
-  })
-
+  });
 });
