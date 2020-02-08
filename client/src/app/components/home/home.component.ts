6d643c0c268b3b4ff17fa4ef83d7673abc6dd9e1
Vlad Drelciuc <vlad.drelciuc@polymtl.ca>
2020-02-07 20:00:26 -0500

    Fix css consistancy on half of the components

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -10,10 +10,7 @@ export class HomeComponent {
     [
       {description : 'Creer un nouveau dessin', optionPath : '/dessin' , show : true},
       {description : 'Ouvrir la Galerie de dessins', optionPath: '/', show : true} ,
-      {description : 'Afficher le guide d\'Utilisation', optionPath: '/userGuide/bienvenue', show : true} ,
+      {description : 'Afficher le Guide d\'utilisation', optionPath: '/userGuide/bienvenue', show : true} ,
       {description : 'Continuer un dessin', show : false}
     ];
-  messageAccueil = 'Bienvenue a PolyDessin';
-  messageDescriptif = "A tout dessin un artiste, et cet artiste, c'est vous!";
-
 }
