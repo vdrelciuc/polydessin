68e8c377a5ba39d5edc02f8691807985fa201cc0
William Decoste <william.decoste@polymtl.ca>
2020-02-08 21:10:43 -0500

    Shift angle will not be omitted when line is out of bounds

--- client/src/app/classes/coordinates-x-y.ts
+++ client/src/app/classes/coordinates-x-y.ts
@@ -37,46 +37,19 @@ export class CoordinatesXY {
     }
   }
 
-  private ajustPointerX(angle: number, pointerX: number, pointerY: number, bisectrixY: number, verticalLimit: number): number {
-    bisectrixY = this.clamp(bisectrixY, verticalLimit);
-    /*if (this.y + this.findYDifferenceForBisectrix(pointerX) > verticalLimit) {
-      return bisectrixY / (Math.tan(angle * Math.PI / 180));
-    } else if (this.y - this.findYDifferenceForBisectrix(pointerX) > verticalLimit) {
-      return bisectrixY / (Math.tan(angle * Math.PI / 180));
-    }
-    return pointerX;*/
-    //return bisectrixY / (Math.tan(angle * 180 / Math.PI));
-    //return bisectrixY * Math.atan(angle * 180 / Math.PI);
-
-    if (this.y + this.findYDifferenceForBisectrix(pointerX) > verticalLimit) {
-      console.log("1: " + (pointerY - this.y - bisectrixY) * Math.tan(angle * Math.PI / 180));
-      return (pointerY - this.y - bisectrixY) / Math.tan(angle * Math.PI / 180);
-    } else if (this.y - this.findYDifferenceForBisectrix(pointerX) < 0) {
-      console.log("2: " + (pointerY - this.y - bisectrixY) * Math.tan(angle * Math.PI / 180));
-      return (pointerY - this.y - bisectrixY) / Math.tan(angle * Math.PI / 180);
-    }
-    return 0;
-  }
-
   private getShiftedPoint(angle: number, pointerX: number, pointerY: number, bisectrixY: number, verticalLimit: number): CoordinatesXY {
-    if (angle < 45 / 2 /*|| this.checkIfHeightIsCorrect(bisectrixY, verticalLimit - this.y)*/) {
+    if (angle < 45 / 2 || this.isOutOfBounds(bisectrixY, verticalLimit)) {
       return new CoordinatesXY(pointerX, this.y);
     } else {
       if (angle <  3 * (90 / 4)) {
-        //pointerX = this.ajustPointerX(angle, pointerX, pointerY, bisectrixY, verticalLimit)
-        return new CoordinatesXY(pointerX, this.clamp(bisectrixY /*- this.ajustPointerX(angle, pointerX, pointerY, bisectrixY, verticalLimit)*/, verticalLimit));
+        return new CoordinatesXY(pointerX, bisectrixY);
       }
       return new CoordinatesXY(this.x, pointerY);
     }
   }
 
-  private checkIfHeightIsCorrect(value: number, verticalLimit: number): boolean {
-    console.log(value > 0 && value < verticalLimit);
-    return (value > 0 && value < verticalLimit);
-  }
-
-  private clamp(value: number, verticalLimit: number): number {
-    return Math.min(Math.max(value, 0), verticalLimit);
+  private isOutOfBounds(value: number, verticalLimit: number): boolean {
+    return (value < 0 || value > verticalLimit);
   }
 
   private findYDifferenceForBisectrix(pointerX: number): number {
