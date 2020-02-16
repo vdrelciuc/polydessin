246204bc5b2d6a036d8481935e039a3f1d436257
William Decoste <william.decoste@polymtl.ca>
2020-02-16 14:44:33 -0500

    Adding EllipseComponent tests

--- client/src/app/components/ellipse/ellipse.component.spec.ts
+++ client/src/app/components/ellipse/ellipse.component.spec.ts
@@ -1,16 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
 
 import { EllipseComponent } from './ellipse.component';
+import { Renderer2, Type } from '@angular/core';
+import { HotkeysService } from 'src/app/services/events/shortcuts/hotkeys.service';
+import { EllipseService } from 'src/app/services/index/drawable/ellipse/ellipse.service';
+import { ToolSelectorService } from 'src/app/services/tools/tool-selector.service';
+import { DrawablePropertiesService } from 'src/app/services/index/drawable/properties/drawable-properties.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatInputModule, MatSlideToggleModule } from '@angular/material';
 
 describe('EllipseComponent', () => {
   let component: EllipseComponent;
   let fixture: ComponentFixture<EllipseComponent>;
+  const mockedRendered = (parentElement: any, name: string, debugInfo?: any): Element => {
+    const element = new Element();
+    parentElement.children.push(element);
+    return element;
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EllipseComponent ]
+      declarations: [ EllipseComponent ],
+      providers: [
+        {
+          provide: Renderer2,
+          useValue: {
+              createElement: () => mockedRendered,
+              setAttribute: () => mockedRendered,
+              appendChild: () => mockedRendered,
+              removeChild: () => mockedRendered,
+          },
+        },
+        HotkeysService,
+        EllipseService,
+        ToolSelectorService,
+        DrawablePropertiesService
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        MatSliderModule,
+        MatFormFieldModule,
+        MatOptionModule,
+        MatSelectModule,
+        MatInputModule,
+        MatSlideToggleModule,
+      ]
     })
     .compileComponents();
+    fixture = TestBed.createComponent(EllipseComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+    component['service']['manipulator'] = getTestBed().get<Renderer2>(Renderer2 as Type<Renderer2>);
   }));
 
   beforeEach(() => {
