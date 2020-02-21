7f5eab435dcea5180844391452fece355571f751
JoeA <joe-abdo3@hotmail.com>
2020-02-20 20:51:50 -0500

    Testing Eraser

--- client/src/app/services/index/drawable/eraser/eraser.service.spec.ts
+++ client/src/app/services/index/drawable/eraser/eraser.service.spec.ts
@@ -1,12 +1,69 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { EraserService } from './eraser.service';
+import { Renderer2, ElementRef, Type } from '@angular/core';
+import { ColorSelectorService } from 'src/app/services/color-selector.service';
+import { DrawStackService } from 'src/app/services/tools/draw-stack/draw-stack.service';
+import * as CONSTANTS from '../../../../classes/constants';
 
 describe('EraserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: EraserService;
+  let manipulator: Renderer2;
+  let image: ElementRef<SVGPolylineElement>;
+  const mockedRendered = (parentElement: any, name: string, debugInfo?: any): Element => {
+    const element = new Element();
+    parentElement.children.push(element);
+    return element;
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Renderer2,
+          useValue: {
+              createElement: () => mockedRendered,
+              setAttribute: () => mockedRendered,
+              appendChild: () => mockedRendered,
+              removeChild: () => mockedRendered,
+          },
+      },
+      {
+        provide: ElementRef,
+        useValue: {
+            nativeElement: {
+                getBoundingClientRect: () => {
+                    const boundleft = 0;
+                    const boundtop = 0;
+                    const boundRect = {
+                        left: boundleft,
+                        top: boundtop,
+                    };
+                    return boundRect;
+                },
+                clientHeight: 100,
+            },
+          },
+        },
+        ColorSelectorService,
+        DrawStackService
+      ],
+    });
+    service = TestBed.get(EraserService);
+    manipulator = getTestBed().get<Renderer2>(Renderer2 as Type<Renderer2>);
+    image = getTestBed().get<ElementRef>(ElementRef as Type<ElementRef>)
+    service.initialize(manipulator, image, 
+      getTestBed().get<ColorSelectorService>(ColorSelectorService as Type<ColorSelectorService>),
+      getTestBed().get<DrawStackService>(DrawStackService as Type<DrawStackService>));
+  });
 
   it('should be created', () => {
-    const service: EraserService = TestBed.get(EraserService);
     expect(service).toBeTruthy();
   });
+
+  it('should set default properties', () => {
+    service.initializeProperties();
+    expect(service.thickness).toEqual(CONSTANTS.THICKNESS_MINIMUM_ERASER);
+  });
+  
 });
