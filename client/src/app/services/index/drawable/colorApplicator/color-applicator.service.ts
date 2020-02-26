006d70ab1792c6bb9687b64b544cf2a9f2a644ca
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-02-26 10:09:58 -0500

    added wrong core element for the onclick function of the colorApplicator

--- client/src/app/services/index/drawable/colorApplicator/color-applicator.service.ts
+++ client/src/app/services/index/drawable/colorApplicator/color-applicator.service.ts
@@ -3,6 +3,7 @@ import { DrawableService } from '../drawable.service';
 import { ColorSelectorService } from '../../../color-selector.service';
 import { DrawStackService } from '../../../tools/draw-stack/draw-stack.service';
 import { DrawablePropertiesService } from '../properties/drawable-properties.service';
+import { CoordinatesXY } from '../../../../classes/coordinates-x-y';
 
 @Injectable({
   providedIn: 'root'
@@ -37,7 +38,20 @@ export class ColorApplicatorService extends DrawableService {
   }
 
   onClick(event: MouseEvent): void {
+    let elementOnTop = this.drawStack.findTopElementAt(new CoordinatesXY(
+      event.clientX,
+      event.clientY));
+    if(elementOnTop !== undefined) {
 
+        this.manipulator.setAttribute(this.selectedElement.target.firstChild, SVGProperties.color, this.oldBorder);
+        this.selectedElement = elementOnTop;
+
+      const color = this.selectedElement.target.getAttribute(SVGProperties.color); // NO I HAVE TO THE COLOR OF .firstChild
+      if(color !== null) {
+        this.oldBorder = color;
+      }
+      this.manipulator.setAttribute(this.selectedElement.target.firstChild, SVGProperties.color, CONSTANTS.ERASER_OUTLINE);
+    }
 
   }
 }
