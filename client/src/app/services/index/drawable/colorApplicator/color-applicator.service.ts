87ca0ea2e3e5a2affa6026cda38574d3d0a26f5b
abdelrahman bassiouni <abdelrahman.mohamed-bassiouni@polymtl.ca>
2020-02-26 10:46:48 -0500

    The color Applicator works for the primaryColor!!!

--- client/src/app/services/index/drawable/colorApplicator/color-applicator.service.ts
+++ client/src/app/services/index/drawable/colorApplicator/color-applicator.service.ts
@@ -4,11 +4,16 @@ import { ColorSelectorService } from '../../../color-selector.service';
 import { DrawStackService } from '../../../tools/draw-stack/draw-stack.service';
 import { DrawablePropertiesService } from '../properties/drawable-properties.service';
 import { CoordinatesXY } from '../../../../classes/coordinates-x-y';
+import { SVGProperties } from '../../../../classes/svg-properties';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorApplicatorService extends DrawableService {
+
+
+
+
   initialize(manipulator: Renderer2,
              image: ElementRef<SVGElement>,
              colorSelectorService: ColorSelectorService,
@@ -41,16 +46,17 @@ export class ColorApplicatorService extends DrawableService {
     let elementOnTop = this.drawStack.findTopElementAt(new CoordinatesXY(
       event.clientX,
       event.clientY));
+    const colorFill = this.colorSelectorService.primaryColor.getValue().getHex();
+    const colorBorder = this.colorSelectorService.secondaryColor.getValue().getHex();
+
     if(elementOnTop !== undefined) {
-
-        this.manipulator.setAttribute(this.selectedElement.target.firstChild, SVGProperties.color, this.oldBorder);
-        this.selectedElement = elementOnTop;
-
-      const color = this.selectedElement.target.getAttribute(SVGProperties.color); // NO I HAVE TO THE COLOR OF .firstChild
-      if(color !== null) {
-        this.oldBorder = color;
-      }
-      this.manipulator.setAttribute(this.selectedElement.target.firstChild, SVGProperties.color, CONSTANTS.ERASER_OUTLINE);
+        if (event.button === 0 )
+        this.manipulator.setAttribute(elementOnTop.target.firstChild, SVGProperties.fill, colorFill);
+        else if (event.button === 2) {
+          if (SVGProperties.color !==undefined ){
+            this.manipulator.setAttribute(elementOnTop.target.firstChild, SVGProperties.color, colorBorder);
+          }
+        }
     }
 
   }
