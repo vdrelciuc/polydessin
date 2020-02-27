15cff975bbad8f6579039b322c247c3e54ed995a
Vlad Drelciuc <vdrelciuc@gmail.com>
2020-02-27 15:34:18 -0500

    Connect image to export

--- client/src/app/services/export/export.service.ts
+++ client/src/app/services/export/export.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExportService {
 
-  constructor() { }
+  private image: ElementRef<SVGElement>;
+
+  initialize(image: ElementRef<SVGElement>): void {
+    this.image = image;
+    this.image = this.image; // TODO: delete this line
+  }
 
 }
