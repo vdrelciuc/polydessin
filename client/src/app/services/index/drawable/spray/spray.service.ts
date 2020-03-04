a7718dc0d97197ae531b2eab62e8fa71e6cd3aef
Songtao <songtao.zhang@polymtl.ca>
2020-03-03 20:22:40 -0500

    functionnal spray

--- client/src/app/services/index/drawable/spray/spray.service.ts
+++ client/src/app/services/index/drawable/spray/spray.service.ts
@@ -14,10 +14,14 @@ import * as CONSTANTS from 'src/app/classes/constants';
 })
 export class SprayService extends DrawableService {
   private readonly DOT_RADIUS: number = 1;
+  private readonly DOTS_PER_SPRAY: number = 20;
   private readonly DEFAULT_RADIUS: number = 5;
   private readonly DEFAULT_FREQUENCY: number = 4;
+  private readonly MS_PER_S: number = 1000;
+  private spraying = false;
   private color: Color;
   private opacity: number;
+  private mousePosition: CoordinatesXY;
   radius: number;
   frequency: number;
   private isDrawing: BehaviorSubject<boolean>;
@@ -40,24 +44,24 @@ export class SprayService extends DrawableService {
       this.initializeProperties();
       this.isDrawing.subscribe(
         () => {
-        if(!this.isDrawing.value && this.subElement !== undefined) {
-          this.pushElement();
+          if(!this.isDrawing.value && this.subElement !== undefined) {
+            this.pushElement();
+          }
         }
-      }
       )
     }
 
-    initializeProperties(): void {
+  initializeProperties(): void {
 
-      this.colorSelectorService.primaryColor.subscribe((color: Color) => {
-        this.color = color;
-      });
+    this.colorSelectorService.primaryColor.subscribe((color: Color) => {
+      this.color = color;
+    });
 
-      this.colorSelectorService.primaryTransparency.subscribe((opacity: number) => {
-        this.opacity = opacity;
-      });
+    this.colorSelectorService.primaryTransparency.subscribe((opacity: number) => {
+      this.opacity = opacity;
+    });
 
-    }
+  }
 
   onMouseInCanvas(event: MouseEvent): void {
 
@@ -69,11 +73,14 @@ export class SprayService extends DrawableService {
   }
   onMousePress(event: MouseEvent): void {
     this.isDrawing.next(true);
-    this.beginDraw(event.clientX, event.clientY);
     this.subElement = this.manipulator.createElement('g', 'http://www.w3.org/2000/svg');
     this.manipulator.setAttribute(this.subElement, SVGProperties.title, 'spray');
 
     this.manipulator.appendChild(this.image.nativeElement, this.subElement);
+    if (!this.spraying) {
+      this.spraying = true;
+      this.Spray();
+    }
 
   }
   onMouseRelease(event: MouseEvent): void {
@@ -84,8 +91,7 @@ export class SprayService extends DrawableService {
     }
   }
   onMouseMove(event: MouseEvent): void {
-    if (this.isDrawing.value) {
-    }
+    this.mousePosition = CoordinatesXY.getEffectiveCoords(this.image, event);
   }
 
   endTool(): void {
@@ -96,7 +102,32 @@ export class SprayService extends DrawableService {
     this.isDrawing.next(false);
   }
 
-  private beginDraw(clientX: number, clientY: number) {
+  private printSpray(): void {
+    for (let i = 0; i < this.DOTS_PER_SPRAY; i++) {
+      const radius = this.radius * this.generateRandom();
+      const angle = 2 * Math.PI * this.generateRandom();
+      this.createDot(this.mousePosition.getX() + radius * Math.cos(angle), this.mousePosition.getY() + radius * Math.sin(angle));
+    }
+  }
+  private generateRandom(): number {
+    return Math.random();
+  }
+  private createDot(x: number, y: number): void {
+    const dot = this.manipulator.createElement(SVGProperties.circle, 'http://www.w3.org/2000/svg');
+    this.manipulator.setAttribute(dot, SVGProperties.fill, this.color.getHex());
+    this.manipulator.setAttribute(dot, SVGProperties.globalOpacity, this.opacity.toString());
+    this.manipulator.setAttribute(dot, SVGProperties.radius, (this.DOT_RADIUS / 2).toString());
+    this.manipulator.setAttribute(dot, SVGProperties.centerX, x.toString());
+    this.manipulator.setAttribute(dot, SVGProperties.centerY, y.toString());
+    this.manipulator.appendChild(this.subElement, dot);
   }
 
+  async Spray(): Promise<void> {
+    if (this.isDrawing.value) {
+      this.printSpray();
+      setTimeout(() => this.Spray(), this.MS_PER_S / this.frequency);
+    } else {
+      this.spraying = false;
+    }
+  }
 }
