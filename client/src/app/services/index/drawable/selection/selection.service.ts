1189d12c72344c03b3ef076a9819a851308b3c4f
William Decoste <william.decoste@polymtl.ca>
2020-03-06 23:40:52 -0500

    Removing dead code and comments in selection

--- client/src/app/services/index/drawable/selection/selection.service.ts
+++ client/src/app/services/index/drawable/selection/selection.service.ts
@@ -11,7 +11,6 @@ import { Stack } from 'src/app/classes/stack';
 import { SelectionTransformShortcutService } from './selection-transform-shortcut.service';
 import { Transform } from 'src/app/classes/transformations';
 import { SelectionState } from 'src/app/enums/selection-states';
-//import { InvertedElement } from 'src/app/interfaces/InvertedElement';
 
 @Injectable({
   providedIn: 'root'
@@ -20,9 +19,7 @@ export class SelectionService extends DrawableService {
 
   private readonly CONTROLPOINT_SIZE = 6;
 
-  private mousePosition: CoordinatesXY;
   private selectionOrigin: CoordinatesXY;
-  //private clickedElement: SVGGElement | null;
   private clickedElement: SVGElementInfos | null;
 
   private state: SelectionState = SelectionState.idle;
@@ -34,14 +31,10 @@ export class SelectionService extends DrawableService {
   private selectedElements: Stack<SVGElementInfos>;
   private selectionRect: SVGRectElement;
   private selectionGroup: SVGGElement;
-
   private controlPoints: SVGRectElement[];
 
-  private elementsToInvert: Stack<SVGElementInfos>;
-  private addedElements: Stack<SVGElementInfos>;
-  private removedElements: Stack<SVGElementInfos>;
-
   private oldMousePosition: CoordinatesXY;
+  private elementsToInvert: Stack<SVGElementInfos>;
 
   private transformShortcuts: SelectionTransformShortcutService;
 
@@ -73,57 +66,11 @@ export class SelectionService extends DrawableService {
     this.transformShortcuts.deleteShortcuts();
   }
 
+  onMouseOutCanvas(event: MouseEvent): void {
+    this.onMouseRelease(event);
+  }
+
   onMousePress(event: MouseEvent): void {
-    /*if (this.isChanging) {
-      // This case happens if the mouse button was released out of canvas: the shaped is confirmed on next mouse click
-      this.onMouseRelease(event);
-    } else {
-      if (this.subElement !== undefined) {
-        this.manipulator.removeChild(this.image.nativeElement, this.subElement);
-      }
-
-      this.isChanging = true;
-      this.isSingleClick = true;
-      this.isLeftClick = event.button === 0 || this.selectedElements.isEmpty();
-      this.elementsToInvert = new Stack<SVGElementInfos>();
-      this.addedElements = new Stack<SVGElementInfos>();
-      this.removedElements = new Stack<SVGElementInfos>();
-
-      this.selectionOrigin = CoordinatesXY.getEffectiveCoords(this.image, event);
-      this.mousePosition = CoordinatesXY.getEffectiveCoords(this.image, event);
-      //this.clickedElement = (event.target as SVGGElement).parentNode as SVGGElement;
-      this.clickedElement = event.target as SVGElement;
-
-
-      this.selectionBox = new DOMRect(this.selectionOrigin.getX() + this.image.nativeElement.getBoundingClientRect().left, this.selectionOrigin.getY() + this.image.nativeElement.getBoundingClientRect().top);
-      this.setupProperties();
-
-      const absoluteMousePosition = new CoordinatesXY(
-        this.mousePosition.getX() + this.image.nativeElement.getBoundingClientRect().left,
-        this.mousePosition.getY() + this.image.nativeElement.getBoundingClientRect().top
-      );
-
-      if (this.clickedElement !== null && this.clickedElement.getAttribute('title') === 'control-point' && this.isLeftClick) {
-        // Resize -> Sprint 3
-        this.isResizing = true;
-        this.isSingleClick = false;
-      } else if (this.clickedElement !== null && this.isInSelectionArea(absoluteMousePosition) && this.isLeftClick) {
-        
-        const topElement = this.drawStack.findTopElementAt(absoluteMousePosition);
-
-        if (topElement !== undefined && this.selectedElements.contains(topElement)) {
-          // Move selection
-          this.oldPointerOnMove = CoordinatesXY.getEffectiveCoords(this.image, event);
-          this.selectionIsMoving = true;
-        }
-      } else if (!this.isLeftClick || (this.clickedElement !== null && !this.isInSelectionArea(absoluteMousePosition))) {
-        // Selection of elements if not in selection area
-        this.isLeftClick ? this.addTopElement() : this.invertTopElement();
-        this.setGeneratedAreaBorders();
-      }
-
-    }*/
-
     this.selectionOrigin = CoordinatesXY.getEffectiveCoords(this.image, event);
     this.oldMousePosition = this.selectionOrigin;
 
@@ -134,6 +81,7 @@ export class SelectionService extends DrawableService {
         break;
       }
     }
+
     const target = (event.target as SVGGElement).parentNode as SVGGElement;
     this.clickedElement = null;
     for (let groupElement of this.drawStack.getAll().getAll()) {
@@ -142,10 +90,8 @@ export class SelectionService extends DrawableService {
         break;
       }
     }
-    console.log(this.clickedElement);
 
     if (this.state !== SelectionState.idle) {
-      // This case happens if the mouse button was released out of canvas: the shaped is confirmed on next mouse click
       this.onMouseRelease(event);
     } else if (event.button === 0) {
       // Left click
@@ -173,22 +119,6 @@ export class SelectionService extends DrawableService {
   }
 
   onMouseRelease(event: MouseEvent): void {
-    /*if (this.isSingleClick && this.isLeftClick && this.clickedElement !== null && this.clickedElement.getAttribute('title') === 'selection-area') {
-      this.addOrRemoveTopElement();
-      this.setGeneratedAreaBorders();
-    }
-
-    this.isChanging = false;
-    this.selectionIsMoving = false;
-    this.isResizing = false;
-    this.manipulator.removeChild(this.subElement, this.perimeter);
-    this.manipulator.removeChild(this.subElement, this.perimeterAlternative);
-    //this.manipulator.setAttribute(this.selectionRect, CursorProperties.cursor, CursorProperties.move);
-    this.transFormShortcuts.setupShortcuts(this.manipulator);
-    if (this.selectedElements.isEmpty()) {
-      this.cancelSelection();
-    }*/
-
     switch (this.state) {
       case SelectionState.leftClickInSelection:
       case SelectionState.singleLeftClickOutOfSelection:
@@ -201,37 +131,16 @@ export class SelectionService extends DrawableService {
         this.removePerimeter();
         break;
     }
-    
     this.invertSelection();
-      
     this.transformShortcuts.setupShortcuts(this.manipulator);
+    this.state = SelectionState.idle;
+
     if (this.selectedElements.isEmpty()) {
       this.cancelSelection();
     }
-
-    this.state = SelectionState.idle;
   }
 
   onMouseMove(event: MouseEvent): void {
-    /*if (this.isChanging) {
-      this.mousePosition = CoordinatesXY.getEffectiveCoords(this.image, event); // Save mouse position for KeyPress Event
-
-      if (this.selectionIsMoving) {
-        // Moving selection
-        this.changePositionOnMove();
-      } else if (this.isResizing) {
-        // Resizing selection
-        this.resizeOnMove();
-      } else {
-        // Selection of elements
-        this.changeSelectionPerimeterOnMove();
-      }
-    }*/
-
-    /*if (this.state === SelectionState.singleLeftClick) {
-      this.state = SelectionState.selecting;
-    } else if (this.state === )*/
-
     switch (this.state) {
       case SelectionState.singleLeftClickOutOfSelection:
         this.state = SelectionState.selecting;
@@ -267,10 +176,6 @@ export class SelectionService extends DrawableService {
     this.oldMousePosition = new CoordinatesXY(mousePosition.getX(), mousePosition.getY());
   }
 
-  private resizeOnMove(): void {
-    // TODO: Sprint 3
-  }
-
   private updateSelectionRect(mousePosition: CoordinatesXY): void {
     let width = Math.abs(mousePosition.getX() - this.selectionOrigin.getX());
     let height = Math.abs(mousePosition.getY() - this.selectionOrigin.getY());
@@ -279,17 +184,15 @@ export class SelectionService extends DrawableService {
     const boxOrigin = new CoordinatesXY(Math.min(this.selectionOrigin.getX(), mousePosition.getX()), Math.min(this.selectionOrigin.getY(), mousePosition.getY()));
     this.selectionBox = new DOMRect(boxOrigin.getX() + this.image.nativeElement.getBoundingClientRect().left, boxOrigin.getY() + this.image.nativeElement.getBoundingClientRect().top, width, height);
 
-    // Set dimensions attributes for perimeter
-    this.manipulator.setAttribute(this.perimeter, SVGProperties.width, width.toString());
-    this.manipulator.setAttribute(this.perimeter, SVGProperties.height, height.toString());
-    this.manipulator.setAttribute(this.perimeterAlternative, SVGProperties.width, width.toString());
-    this.manipulator.setAttribute(this.perimeterAlternative, SVGProperties.height, height.toString());
-
-    // Set dimensions attributes for perimeter
+    // Set dimensions attributes for perimeter and align origin
     this.manipulator.setAttribute(this.perimeter, SVGProperties.x, boxOrigin.getX().toString());
     this.manipulator.setAttribute(this.perimeterAlternative, SVGProperties.x, boxOrigin.getX().toString());
     this.manipulator.setAttribute(this.perimeter, SVGProperties.y, boxOrigin.getY().toString());
     this.manipulator.setAttribute(this.perimeterAlternative, SVGProperties.y, boxOrigin.getY().toString());
+    this.manipulator.setAttribute(this.perimeter, SVGProperties.width, width.toString());
+    this.manipulator.setAttribute(this.perimeterAlternative, SVGProperties.width, width.toString());
+    this.manipulator.setAttribute(this.perimeter, SVGProperties.height, height.toString());
+    this.manipulator.setAttribute(this.perimeterAlternative, SVGProperties.height, height.toString());
   }
 
   private setupProperties(): void {
@@ -345,20 +248,13 @@ export class SelectionService extends DrawableService {
   }
 
   private appendPerimeter(): void {
-    // Adding perimeter to DOM
     this.manipulator.appendChild(this.subElement, this.perimeter);
     this.manipulator.appendChild(this.subElement, this.perimeterAlternative);
   }
 
   private removePerimeter(): void {
-    // Removing perimeter from DOM
     this.manipulator.removeChild(this.subElement, this.perimeter);
     this.manipulator.removeChild(this.subElement, this.perimeterAlternative);
-  }
-
-  private createSelectionRect(): void {
-    //this.manipulator.setAttribute(this.selectionRect, CursorProperties.cursor, CursorProperties.default);
-    this.manipulator.appendChild(this.image.nativeElement, this.subElement);
   }
 
   private onSingleClick(): void {
@@ -379,81 +275,16 @@ export class SelectionService extends DrawableService {
     }
     
     this.setGeneratedAreaBorders();
-
-
-    /*const absoluteMousePosition = new CoordinatesXY(
-      this.mousePosition.getX() + this.image.nativeElement.getBoundingClientRect().left,
-      this.mousePosition.getY() + this.image.nativeElement.getBoundingClientRect().top
-    );
-    const topElement = this.drawStack.findTopElementAt(absoluteMousePosition);
-
-    if (topElement !== undefined) {
-      this.selectedElements.push_back(topElement);
-    }*/
-
-    /*
-    // Avec event.target
-    if (this.clickedElement !== null) {
-      const id = this.clickedElement.getAttribute('title');
-      if (id !== null && this.clickedElement.tagName === 'g') {
-        console.log(this.clickedElement);
-        const topElement: SVGElementInfos = { target: this.clickedElement, id: parseInt(id) };
-        this.selectedElements.push_back(topElement);
-      }
-    }*/
   }
 
-  private invertSelection() {
+  private invertSelection(): void {
     for (let element of this.elementsToInvert.getAll()) {
-      if (this.selectedElements.contains(element)) {
-        this.selectedElements.delete(element);
-      } else {
-        this.selectedElements.push_back(element);
-      }
+      this.selectedElements.contains(element) ? this.selectedElements.delete(element) : this.selectedElements.push_back(element);
     }
-
     this.elementsToInvert = new Stack<SVGElementInfos>();
   }
 
-  private invertTopElement(): void {
-    const absoluteMousePosition = new CoordinatesXY(
-      this.mousePosition.getX() + this.image.nativeElement.getBoundingClientRect().left,
-      this.mousePosition.getY() + this.image.nativeElement.getBoundingClientRect().top
-    );
-    const topElement = this.drawStack.findTopElementAt(absoluteMousePosition);
-
-    if (topElement !== undefined && !this.selectedElements.contains(topElement)) {
-      this.selectedElements.push_back(topElement);
-    } else if (topElement !== undefined) {
-      this.selectedElements.delete(topElement);
-    }
-
-    /*
-    // Avec event.target
-    if (this.clickedElement !== null) {
-      const id = this.clickedElement.getAttribute('title');
-      if (id !== null && this.clickedElement.tagName === 'g') {
-        console.log(this.clickedElement);
-        const topElement: SVGElementInfos = { target: this.clickedElement, id: parseInt(id) };
-        if (!this.selectedElements.contains(topElement)) {
-          this.selectedElements.push_back(topElement);
-        } else {
-          this.selectedElements.delete(topElement);
-        }
-      }
-    }*/
-  }
-
   private addOrInvertEachElementInRect(): void {
-    /*this.selectedElements = new Stack<SVGElementInfos>();
-
-    for (let i = 0; i < this.drawStack.size(); i++) {
-      const element = this.drawStack.hasElementIn(i, this.selectionBox);
-      if (element !== undefined) {
-        this.selectedElements.push_back(element);
-      }
-    }*/
-
     let stack = new Stack<SVGElementInfos>();
 
     for (let i = 0; i < this.drawStack.size(); i++) {
@@ -463,88 +294,18 @@ export class SelectionService extends DrawableService {
       }
     }
 
-    if (this.state === SelectionState.selecting) {
-      this.selectedElements = stack;
-    } else {
-      this.elementsToInvert = stack;
-    }
-
+    this.state === SelectionState.selecting ? this.selectedElements = stack : this.elementsToInvert = stack;
     this.setGeneratedAreaBorders();
-  }
-
-  private invertEachElement(): void {
-    this.elementsToInvert = new Stack<SVGElementInfos>();
-
-    for (let i = 0; i < this.drawStack.size(); i++) {
-      const element = this.drawStack.hasElementIn(i, this.selectionBox);
-      if (element !== undefined) {
-        this.elementsToInvert.push_back(element);
-
-        const isAlreadySelected = this.selectedElements.contains(element);
-        const isAlreadyRemovedOrAdded = isAlreadySelected ? this.addedElements.contains(element) : this.removedElements.contains(element);
-
-        if (!isAlreadyRemovedOrAdded) {
-          isAlreadySelected ? this.selectedElements.delete(element) : this.selectedElements.push_back(element);
-          isAlreadySelected ? this.removedElements.push_back(element) : this.addedElements.push_back(element);
-        }
-      }
-    }
-
-    for (let element of this.drawStack.getAll().getAll()) {
-      if (this.addedElements.contains(element) && !this.elementsToInvert.contains(element)) {
-        this.addedElements.delete(element);
-        this.selectedElements.delete(element);
-      } else if (this.removedElements.contains(element) && !this.elementsToInvert.contains(element)) {
-        this.removedElements.delete(element);
-        this.selectedElements.push_back(element);
-      }
-    }
   }
 
   selectAllElements(): void {
-    this.cancelSelection();
-
-    //this.selectedElements = this.drawStack.getAll();
-    this.selectedElements = new Stack<SVGElementInfos>();
-    for (let i = 0; i < this.drawStack.size(); i++) {
-      this.selectedElements.push_back(this.drawStack.getAll().getAll()[i]);
-    }
-    //this.createSelectionRect();
-    //this.manipulator.setAttribute(this.selectionRect, CursorProperties.cursor, CursorProperties.move);
+    this.selectedElements = this.drawStack.getAll();
     this.setGeneratedAreaBorders();
-    if (this.selectedElements.isEmpty()) {
-      this.cancelSelection();
-    }
+    this.manipulator.appendChild(this.image.nativeElement, this.subElement);
     this.transformShortcuts.setupShortcuts(this.manipulator);
   }
 
   private setGeneratedAreaBorders(): void {
-    /*const elementBorder = element.target.getBoundingClientRect();
-
-    const firstElementOfGroup = element.target.firstChild as SVGElement;
-    const borderProperty = (firstElementOfGroup.tagName !== 'polyline') ? firstElementOfGroup.getAttribute(SVGProperties.thickness) : null;
-    const borderThickness = parseInt((borderProperty !== null) ? borderProperty : '0') / 2;
-
-    if (this.generatedArea === undefined) {
-      this.generatedArea = {
-        left: elementBorder.left - borderThickness,
-        right: elementBorder.right + borderThickness,
-        top: elementBorder.top + borderThickness,
-        bottom: elementBorder.bottom - borderThickness
-      };
-    }
-    if (elementBorder.left - borderThickness < this.generatedArea.left) {
-      this.generatedArea.left = elementBorder.left - borderThickness;
-    }
-    if (elementBorder.right + borderThickness > this.generatedArea.right) {
-      this.generatedArea.right = elementBorder.right + borderThickness;
-    }
-    if (elementBorder.top - borderThickness < this.generatedArea.top) {
-      this.generatedArea.top = elementBorder.top - borderThickness;
-    }
-    if (elementBorder.bottom + borderThickness > this.generatedArea.bottom) {
-      this.generatedArea.bottom = elementBorder.bottom + borderThickness;
-    }*/
     let selection: SVGElementInfos[] = [];
     for (const element of this.selectedElements.getAll()) {
       selection.push(element);
@@ -552,13 +313,8 @@ export class SelectionService extends DrawableService {
     
     for (let element of this.elementsToInvert.getAll()) {
       const indexToRemove = selection.indexOf(element);
-      if (indexToRemove !== -1) {
-        selection.splice(indexToRemove, 1);
-      } else {
-        selection.push(element);
-      }
+      indexToRemove === -1 ? selection.push(element) : selection.splice(indexToRemove, 1);
     }
-  
 
     if (selection.length > 0) {
       const firstElement = selection[0].target.getBoundingClientRect();
