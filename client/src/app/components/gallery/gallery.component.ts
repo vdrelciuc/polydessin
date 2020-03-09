e61c445f44a593394417049ef065e74b1576d433
abdelrahman bassiouni <abdelrahman.bassiouni@gmail.com>
2020-03-09 00:35:49 -0400

    Filter made and binded correctly to gallery, functionnal, still need to test if shows multiple images with the same tags

--- client/src/app/components/gallery/gallery.component.ts
+++ client/src/app/components/gallery/gallery.component.ts
@@ -11,7 +11,7 @@ import {DomSanitizer} from "@angular/platform-browser";
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit{
+export class GalleryComponent implements OnInit {
 
 
   isSearchByTags: boolean;
@@ -19,27 +19,28 @@ export class GalleryComponent implements OnInit{
   tags: Set<string>;
   tagName: string;
   images: Image[];
-  resultImages : Image[];
+  resultImages: Image[];
 
   constructor(private dialogRef: MatDialogRef<GalleryComponent>,
               private saveService: SaveServerService,
-              private snacks : MatSnackBar,
-              private imageRetriever : ImagesManagerService,
-              private sanitizer : DomSanitizer
-              ) {
+              private snacks: MatSnackBar,
+              private imageRetriever: ImagesManagerService,
+              private sanitizer: DomSanitizer
+  ) {
     this.isSearchByTags = true;
-    this.tags = new  Set<string>();
+    this.tags = new Set<string>();
   }
 
   ngOnInit(): void {
     this.imageRetriever.getALLImages()
       .subscribe(data => {
         this.images = data;
+        this.resultImages = this.images;
         // bug fix to images url unsafe from https://github.com/angular/angular/issues/18950
-        this.images.forEach((e)=>{
+        this.images.forEach((e) => {
           e.serial = this.sanitizer.bypassSecurityTrustResourceUrl(e.serial) as string;
         });
-        this.snacks.open('Nous avons récupéré les image :)', '' , {duration : 2000} );
+        this.snacks.open('Nous avons récupéré les image :)', '', {duration: 2000});
       });
   }
 
@@ -54,11 +55,36 @@ export class GalleryComponent implements OnInit{
 
   addTag(tag: string): void {
     this.isValidTag = this.saveService.addTag(tag, this.tags);
+    if (this.isValidTag) {
+      this.filterWithTag();
+    }
   }
 
   removeTag(tag: string): void {
     this.isValidTag = this.saveService.removeTag(tag, this.tags);
+    this.filterWithTag();
   }
 
+  filterWithTag(): void {
+    if (this.tags.size === 0) {
+      this.resultImages = this.images;
+      return;
+    }
+    this.resultImages = [];
+    // could have used a forEach but would add the same image more than one time if it has more
+    // than one corresponding ticket because u cant break a foreach loop in typescript
+    for (let i = 0; i < this.images.length; i++) {
+      for (let j = 0; j < this.images[i].tags.length; j++) {
+        if (this.tags.has(this.images[i].tags[j])) {
+          this.resultImages.push(this.images[i]);
+          break;
+        }
+      }
+    }
+
+    if (this.resultImages.length === 0) {
+      this.snacks.open('Aucun dessin correspond a votre recherche ', '', {duration: 3000});
+    }
+  }
 
 }
