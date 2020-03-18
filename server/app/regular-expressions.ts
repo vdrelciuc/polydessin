ee9abc93dea0c755dbe156d4479d38cb3188f586
JoeA <joe-abdo3@hotmail.com>
2020-03-18 19:25:35 -0400

    Constants and regular expressions have also been regrouped on the server side

---
+++ server/app/regular-expressions.ts
@@ -0,0 +1,3 @@
+// Image
+export const REGEX_TITLE: RegExp = /^[A-Za-z0-9- ]{3,16}$/; // Alphanumeric, space and dash: 3 to 16 chars
+export const REGEX_TAG: RegExp = /^[A-Za-z0-9]{1,10}$/; // Alphanumeric, 1 to 10 chars
