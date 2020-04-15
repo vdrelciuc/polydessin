6bd8c26580b299a0c0ecf3cd3a58fabc355ceaaf
Vlad Drelciuc <vlad.drelciuc@polymtl.ca>
2020-04-15 13:56:30 -0400

    Enlever canvasEmpty non utilise

--- client/src/app/services/create-new/create-new.service.ts
+++ client/src/app/services/create-new/create-new.service.ts
@@ -8,6 +8,5 @@ import { CoordinatesXY } from '../../classes/coordinates-x-y';
 export class CreateNewService {
 
   canvasSize: BehaviorSubject<CoordinatesXY> = new BehaviorSubject<CoordinatesXY>(new CoordinatesXY(0, 0));
-  canvasEmpty: boolean;
 
 }
